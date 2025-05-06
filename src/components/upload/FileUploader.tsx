c00a7dbfdec1616edc255c8adf8c91ed8c283ad1
Arun <arun11.kb@gmail.com>
2025-05-06 15:35:07 +0530

    unused varibale in FileUploader.tsx removed

--- src/components/upload/FileUploader.tsx
+++ src/components/upload/FileUploader.tsx
@@ -1,7 +1,6 @@
 import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router';
 import { CloudArrowUpIcon } from '@heroicons/react/24/solid'
-import { useState } from 'react';
 
 type FormValues = {
   fileList: FileList;
